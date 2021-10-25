d6de59071c65051568f528bfcfb55444ddf16ad1
Nathan Panoel <nathanpanoel@gmail.com>
2021-10-25 23:41:09 +0200

    dans métier, je pense restaurant c'est self.ville et pas self.vill + ajout brouillon .env

--- api/metier/adresse.py
+++ api/metier/adresse.py
@@ -2,6 +2,6 @@ class Adresse():
     def __init__(self, adresse : str, code_postal : int, ville : str, pays : str):
         self.adresse = adresse
         self.code_postal = code_postal
-        self.vill = ville
+        self.vill = ville #ville
         self.pays = pays
         
