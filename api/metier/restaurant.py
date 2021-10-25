81e887b4a9bbf8e7d3b93bcfcacb66d87bd74c58
Valentine Moulin <moulin@MacBook-Air-de-MOULIN.local>
2021-10-25 12:45:32 +0200

    modif

--- api/metier/restaurant.py
+++ api/metier/restaurant.py
@@ -1,8 +1,7 @@
 class Restaurant():
-    def __init__(self, id_restaurant : int, nom : str, adresse: Adresse, statut : bool): 
+    def __init__(self, id_restaurant : str, nom : str, adresse: Adresse, statut : bool): 
         self.id_restaurant = id_restaurant
         self.nom = self.nom 
         self.adresse = adresse
         self.statut = statut
-    
     
--- api/service/yelp_mapper.py
+++ api/service/yelp_mapper.py
@@ -5,20 +5,27 @@ from app.model.restaurant import Restaurant
 class YelpMapper:
 
     @staticmethod
-    def businesses_to_restaurants(response) -> Restaurant:
+    def businesses_to_restaurants(response) -> list(Restaurant) :
         # transforme le json en objet metier
         liste_restaurant =[]
         liste_businesses = response['businesses']
         for business in liste_businesses:
-            nom = ""
-            id = ""
-            location = ""
+            nom = business["name"]
+            id = business["id"]
+            location = business["location"]
+            statut = business["is_closed"]
             adresse = YelpMapper.yelp_location_to_location(location)
-            restaurant = Restaurant()
+            restaurant = Restaurant(id= id, nom = nom, location= adresse, statut = statut)
             liste_restaurant.append(restaurant)
         return liste_restaurant
 
     @staticmethod
-    def yelp_location_to_location(response) -> Adresse :
+    def yelp_location_to_location(location : dict) -> Adresse :
         # transforme en objet adresse 
-        raise NotImplementedError
+        for elem in location : 
+            adresse = elem['address1']
+            code_postal = elem['zip_code']
+            pays = elem['country']
+            ville = elem['city']
+            adresse = Adresse(adresse, code_postal, ville, pays)
+        return adresse
