e291e107df4f93f66bcb71378507b8202d835851
Nathan Panoel <nathanpanoel@gmail.com>
2021-10-28 00:42:22 +0200

    mise à jour menu

--- brouillon/scraping_db_building/db_recup_yelp_avis.py
+++ brouillon/scraping_db_building/db_recup_yelp_avis.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 import hashlib
-params = {'location': 'Bruz,Brittany,France',}
+params = {'term': 'food','location': 'Bruz', 'latitude': 48.05089, 'longitude': -1.74192, 'limit':50}
 r=requests.get('https://api.yelp.com/v3/businesses/search',headers={'Authorization': 'Bearer fn70eRh36s5xOBDKdreUKbTNiwwzcMCgh3ydHo8UwT5dSFe_lqle6YLhKN1Xh29LqCf9AmLCmpjksKRa0fWvoWg4RuFS5GhyEvFqMWPMKqILPpx2NHaONy5GOjl4YXYx'},params=params)
 json=r.json()
 df=pd.DataFrame(json['businesses'])
@@ -10,12 +10,11 @@ df=pd.DataFrame(json['businesses'])
 df['id_hash']=df['id'].apply(lambda x: int(hashlib.sha512(x.encode("utf-8")).hexdigest(), 16) % (10 ** 8) )
 list_business_id=(list(df['id']))
 list_business_id_hash=(list(df['id_hash']))
-print(list_business_id)
 
 list_requests=[]
 for address in list_business_id:
-    list_requests.append(requests.get("https://api.yelp.com/v3/businesses/"+address+"/reviews", headers={'Authorization': 'Bearer fn70eRh36s5xOBDKdreUKbTNiwwzcMCgh3ydHo8UwT5dSFe_lqle6YLhKN1Xh29LqCf9AmLCmpjksKRa0fWvoWg4RuFS5GhyEvFqMWPMKqILPpx2NHaONy5GOjl4YXYx'}))   
-r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,r13,r14,r15,r16,r17,r18,r19,r20=list_requests
+    list_requests.append(requests.get("https://api.yelp.com/v3/businesses/"+address+"/reviews", headers={'Authorization': 'Bearer fn70eRh36s5xOBDKdreUKbTNiwwzcMCgh3ydHo8UwT5dSFe_lqle6YLhKN1Xh29LqCf9AmLCmpjksKRa0fWvoWg4RuFS5GhyEvFqMWPMKqILPpx2NHaONy5GOjl4YXYx'},params=params))   
+#r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,r13,r14,r15,r16,r17,r18,r19,r20=list_requests
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 #création des tuples (id_avis,avis,id_restaurant)
@@ -23,7 +22,7 @@ aux=[]
 id_restau,id_avis=0,200
 for restau in list_requests:
     if restau.json()['reviews'] != []:
-        for review in list(pd.DataFrame(r1.json()['reviews'])['text']):
+        for review in list(pd.DataFrame(restau.json()['reviews'])['text']):
             aux.append( (id_avis,review,list_business_id_hash[id_restau]))
             id_avis+=1
     id_restau+=1
--- brouillon/scraping_db_building/recup_yelp_tuple_id_nom_restaurant.py
+++ brouillon/scraping_db_building/recup_yelp_tuple_id_nom_restaurant.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 import hashlib
-params = {'location': 'Bruz,Brittany,France',}
+params = {'term': 'food','location': 'Bruz', 'latitude': 48.05089, 'longitude': -1.74192, 'limit':50}
 r=requests.get('https://api.yelp.com/v3/businesses/search',headers={'Authorization': 'Bearer fn70eRh36s5xOBDKdreUKbTNiwwzcMCgh3ydHo8UwT5dSFe_lqle6YLhKN1Xh29LqCf9AmLCmpjksKRa0fWvoWg4RuFS5GhyEvFqMWPMKqILPpx2NHaONy5GOjl4YXYx'},params=params)
 json=r.json()
 df=pd.DataFrame(json['businesses'])
@@ -25,3 +25,5 @@ for element in result_restaurant:
     file.write(str(element) + ",\n")
 file.write(";")
 file.close()
+
+print(df)
