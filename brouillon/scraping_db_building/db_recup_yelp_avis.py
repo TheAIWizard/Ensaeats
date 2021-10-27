c5286b5366de6ada0e1efd618efa1680ce7790f7
Nathan Panoel <nathanpanoel@gmail.com>
2021-10-27 16:33:51 +0200

    id NUMERIC déf par hachage

--- brouillon/scraping_db_building/db_recup_yelp_avis.py
+++ brouillon/scraping_db_building/db_recup_yelp_avis.py
@@ -1,11 +1,16 @@
 import requests
 import pandas as pd
+import hashlib
 params = {'location': 'Bruz,Brittany,France',}
 r=requests.get('https://api.yelp.com/v3/businesses/search',headers={'Authorization': 'Bearer fn70eRh36s5xOBDKdreUKbTNiwwzcMCgh3ydHo8UwT5dSFe_lqle6YLhKN1Xh29LqCf9AmLCmpjksKRa0fWvoWg4RuFS5GhyEvFqMWPMKqILPpx2NHaONy5GOjl4YXYx'},params=params)
 json=r.json()
 df=pd.DataFrame(json['businesses'])
-df_business_id=df[['id']]
+
+#conversion id en numeric par hachage
+df['id_hash']=df['id'].apply(lambda x: int(hashlib.sha512(x.encode("utf-8")).hexdigest(), 16) % (10 ** 8) )
 list_business_id=(list(df['id']))
+list_business_id_hash=(list(df['id_hash']))
+print(list_business_id)
 
 list_requests=[]
 for address in list_business_id:
@@ -19,12 +24,12 @@ id_restau,id_avis=0,200
 for restau in list_requests:
     if restau.json()['reviews'] != []:
         for review in list(pd.DataFrame(r1.json()['reviews'])['text']):
-            aux.append( (id_avis,review,list_business_id[id_restau]))
+            aux.append( (id_avis,review,list_business_id_hash[id_restau]))
             id_avis+=1
     id_restau+=1
         
 #récupération des résultats dans un fichier .txt
-file = open("db_tuple_avis.txt", "w",encoding="utf-8") 
+file = open("brouillon/scraping_db_building/db_tuple_avis.txt", "w",encoding="utf-8") 
 for element in aux:
     file.write(str(element) + ",\n")
 file.write(";")
