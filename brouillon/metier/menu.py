f330c13e2c527a1008db1d854304a1efc1448704
Pape Tidiane GUEYE <papetidianegueye199@gmail.com>
2021-10-29 14:37:28 +0200

    Objet metier

---
+++ brouillon/metier/menu.py
@@ -0,0 +1,15 @@
+
+class Menu:
+    """Constructeur des menus
+    """
+    def __init__(self, id_menu,
+                 nom_menu,
+                 plat,
+                 boisson,
+                 prix_menu) -> None:
+        self.id_menu = id_menu
+        self.nom_menu = nom_menu
+        self.plat = plat
+        self. boisson = boisson
+        self.prix_menu = prix_menu
+        
