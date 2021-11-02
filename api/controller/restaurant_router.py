e507799d35a818cc542ffb636fe0c0dde0889558
Valentine Moulin <moulin@MacBook-Air-de-MOULIN.local>
2021-11-02 12:40:36 +0100

    modif

--- api/controller/restaurant_router.py
+++ api/controller/restaurant_router.py
@@ -8,26 +8,26 @@ router = APIRouter()
 
 
 @router.get("/restaurants/", tags=["restaurants"])
-def get_restaurants(username: Optional[str] = Header(None), password: Optional[str] = Header(None), localisation:str="Bruz", term : str = ""):
+def get_restaurants(username: Optional[str] = Header(None), password: Optional[str] = Header(None), localisation:str="Bruz", term : str = "", radius : int = 2000):
     try:
         # user = UserService.authenticate_and_get_user(
         #     username=username, password=password)
         # print(user)
         # # call your service here
-        return RestaurantsService.getRestaurants(localisation)
+        return RestaurantsService.getRestaurants(localisation, term, radius)
 
     except UserNotAuthenticated:
         raise HTTPException(status_code=401, detail="User must be logged")
 
 
-@router.get("/restaurants/{id_restaurant}")
-async def get_restaurants(username: Optional[str] = Header(None), password: Optional[str] = Header(None), localisation:str="Bruz"):
+@router.get("/restaurant/{id_restaurant}")
+async def get_restaurant(username: Optional[str] = Header(None), password: Optional[str] = Header(None), id_restaurant: str):
     try:
         # user = UserService.authenticate_and_get_user(
         #     username=username, password=password)
         # print(user)
         # # call your service here
-        return RestaurantsService.getRestaurants(localisation)
+        return RestaurantsService.getRestaurant(id_restaurant)
 
     except UserNotAuthenticated:
         raise HTTPException(status_code=401, detail="User must be logged")
