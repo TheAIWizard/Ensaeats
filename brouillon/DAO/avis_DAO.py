af2faf6b9f2686c69b25b4a5d492f245fba6467c
GHOUMARI Kiyan <id1793@DOMENSAI.ECOLE>
2021-11-02 16:55:31 +0100

    avisdao

--- brouillon/DAO/avis_DAO.py
+++ brouillon/DAO/avis_DAO.py
@@ -1,7 +1,7 @@
 from typing import List
-# from api.metier.avis import Avis
-from utils.singleton import Singleton
-from DAO.db_connection import DBConnection
+from brouillon.metier.avis import Avis
+from brouillon.utils.singleton import Singleton
+from brouillon.DAO.db_connection import DBConnection
 
 
 
@@ -23,7 +23,25 @@ class AvisDao(metaclass=Singleton):
                 res = cursor.fetchone()
         avis_restau = []
         if res :
-            for row in res :
+            for row in res : 
                 avis_restau.append(row["avis"])
         return avis_restau
 
+
+    def add_avis(self, avis : Avis) -> bool:
+        created = False
+
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor :
+                cursor.execute(
+                    "INSERT INTO ensaeats.avis (id_menu, value) VALUES "\
+                    "(%(id_menu)s, %(nom)s, %(prix)s)"\
+                    "RETURNING id_menu;"
+                , {"id_menu" : menu.id_menu
+                  , "name": menu.nom_menu
+                  , "prix": menu.prix_menu})
+                res = cursor.fetchone()
+        if res :
+            menu.id=res['id_menu']
+            created = True
+        return created
