164f424c13421f160472caf55ff79a6ec16b6fd9
Valentine Moulin <moulin@MacBook-Air-de-MOULIN.local>
2021-11-02 16:39:42 +0100

    modif

--- api/service/yelp_mapper.py
+++ api/service/yelp_mapper.py
@@ -1,5 +1,5 @@
-from metier.restaurant import Restaurant
-from metier.adresse import Adresse
+from API.metier.restaurant import Restaurant
+from API.metier.adresse import Adresse
 from typing import List
 
 class YelpMapper:
@@ -22,8 +22,21 @@ class YelpMapper:
         return liste_restaurant
 
     @staticmethod
+    def business_to_restaurant(response) -> Restaurant:
+        # transforme le json en une liste d'objet metier
+        nom = response["name"]
+        id = response["id"]
+        location = response["location"]
+        statut = response["is_closed"]
+        adresse = YelpMapper.yelp_location_to_location(location)
+        restaurant = Restaurant(id_restaurant = id , nom = nom , adresse = adresse, statut = statut)
+            
+        return restaurant
+
+
+    @staticmethod
     def yelp_location_to_location(location : dict) -> str :
-        ''' Transforme en objet Adresse et renvoie en format str '''
+        ''' Transforme en objet Adresse  '''
 
         if location["address1"] is None : 
             adresse = ''
--- start-api.py
+++ start-api.py
@@ -1,6 +1,6 @@
 import uvicorn
 
-from api.app import app
+from API.app import app
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
