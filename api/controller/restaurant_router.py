3dc3eb531f810f0f981126a00368888050f42462
Valentine Moulin <moulin@MacBook-Air-de-MOULIN.local>
2021-11-02 12:25:59 +0100

    modif

--- api/controller/restaurant_router.py
+++ api/controller/restaurant_router.py
@@ -8,7 +8,20 @@ router = APIRouter()
 
 
 @router.get("/restaurants/", tags=["restaurants"])
-def get_restaurants(username: Optional[str] = Header(None), password: Optional[str] = Header(None), localisation:str="Bruz"):
+def get_restaurants(username: Optional[str] = Header(None), password: Optional[str] = Header(None), localisation:str="Bruz", term : str = ""):
+    try:
+        # user = UserService.authenticate_and_get_user(
+        #     username=username, password=password)
+        # print(user)
+        # # call your service here
+        return RestaurantsService.getRestaurants(localisation)
+
+    except UserNotAuthenticated:
+        raise HTTPException(status_code=401, detail="User must be logged")
+
+
+@router.get("/restaurants/{id_restaurant}")
+async def get_restaurants(username: Optional[str] = Header(None), password: Optional[str] = Header(None), localisation:str="Bruz"):
     try:
         # user = UserService.authenticate_and_get_user(
         #     username=username, password=password)
--- api/metier/restaurant.py
+++ api/metier/restaurant.py
@@ -1,7 +1,7 @@
 from API.metier.adresse import Adresse
 
 class Restaurant():
-    def __init__(self, id_restaurant : str, nom : str, adresse: str, statut : bool): 
+    def __init__(self, id_restaurant : str, nom : str, adresse: Adresse, statut : bool): 
         self.id_restaurant = id_restaurant
         self.nom = nom 
         self.adresse = adresse
