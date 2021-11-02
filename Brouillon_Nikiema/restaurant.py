eb46a1526ba55337cb29b7b5d8d9049f8d7e61bc
Nikiema <zoukou1996@gmail.com>
2021-11-02 11:27:59 +0100

    Brouillon Nikiema

--- Brouillon_Nikiema/restaurant.py
+++ Brouillon_Nikiema/restaurant.py
@@ -1,4 +1,3 @@
-from adresse import Adresse
 class Restaurant():
     def __init__(self, id_restaurant : str, nom : str, adresse: Adresse, statut : bool): 
         self.id_restaurant = id_restaurant
