9786b6b87fc95aa9d3bc5954570f69f070e09121
Pape Tidiane GUEYE <papetidianegueye199@gmail.com>
2021-11-03 12:46:12 +0100

    Commande DAO

--- Tidiane_client_brouillon/dao/commande_dao.py
+++ Tidiane_client_brouillon/dao/commande_dao.py
@@ -1,17 +1,17 @@
 from typing import List, Optional
 from brouillon.utils.singleton import Singleton
 from brouillon.DAO.db_connection import DBConnection
-from Tidiane_client_brouillon.metier.panier import Panier
+from brouillon.metier.commande import Commande
 
 class CommandeDAO(metaclass=Singleton):
     inserted = False
    
-    def add_commande(self, id_commande, panier: Panier, date_commande, statut_commande):
+    def add_commande(self, commande: Commande):
         """Ajout d'une commande dans la base de données
         """
         ## penser à tenir compte des id auto increment après
         requete = "INSERT INTO ensaeats.commande (id_commande, date, prix_total, statut_commande) VALUES "\
-            "({}, '{}', {}, '{}') RETURNING id_commande".format(id_commande, date_commande, panier.calcul_total(), statut_commande)
+            "({}, '{}', {}, '{}') RETURNING id_commande".format(commande.id_commande, commande.date, commande.prix_total(), commande.statut_commande)
         
         with DBConnection().connection as connection:
             with connection.cursor() as cursor :
--- brouillon/metier/commande.py
+++ brouillon/metier/commande.py
@@ -3,12 +3,19 @@ class Commande:
     """
     def __init__(self, id_commande: int,
                  date : int,
-                 paiement: float,
                  statut_commande: str,
                  liste_menu: list) -> None:
         self.id_commande = id_commande
         self.date = date
-        self.paiement = paiement
         self.statut_commande = statut_commande
         self.liste_menu = liste_menu
         
+        
+    def prix_total(self):
+        """Calcul du prix total
+        """
+        prix_total = 0
+        for menu in self.liste_menu:
+            prix_total += menu.prix_menu
+            
+        return prix_total
