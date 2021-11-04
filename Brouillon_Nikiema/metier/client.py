ddd921d45afa5647a729cc07e33970f8c7651b86
NIKIEMA Zoulkarnayni <id1712@clust-n2.domensai.ecole>
2021-11-04 12:45:16 +0100

    les modfication de la classe utilisateur

--- Brouillon_Nikiema/metier/client.py
+++ Brouillon_Nikiema/metier/client.py
@@ -1,12 +1,12 @@
 from Brouillon_Nikiema.metier.utilisateur import Utilisateur
 from Brouillon_Nikiema.metier.user import User
 from Brouillon_Nikiema.metier.commande import Commande
+from pydantic.main import BaseModel
 
-
-class Client (Utilisateur) :
-    def __init__(self, user : User, commande : Commande, 
-    nom : str, prenom : str, id_client : int) -> None:
-        self.id_client=id_client
-        self.telephone=telephone
-        super().__init__( nom , prenom , telephone ,role , user, commande) 
-    
+class Client (Utilisateur, BaseModel) :
+    id_client : int
+    telephone : str
+    user : User
+    commande : Commande
+    nom : str
+    prenom : str
