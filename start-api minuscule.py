afaf2eb0f99a6519db5c622776eb066ea3de1d84
Nathan Panoel <nathanpanoel@gmail.com>
2021-11-07 17:40:59 +0100

    authentification restaurateur

--- start-api minuscule.py
+++ start-api minuscule.py
@@ -4,3 +4,12 @@ from api_minuscule.app import app
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
+    """ exemple post
+{
+  "nom": "Delacroix",
+  "prenom": "Martin",
+  "identifiant": "CrossMartin",
+  "mot_de_passe": "1234",
+  "id_restaurant": "LTy9AUgMnLn8YS21KfFZ8g"
+}
+ """
