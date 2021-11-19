d1c8729ed2ec70e735e5a2ae69a0c4200b6f3c35
Nikiema <zoukou1996@gmail.com>
2021-11-19 16:59:58 +0100

    mise a jours des classe du package metier

--- api/metier/commande.py
+++ api/metier/commande.py
@@ -11,10 +11,29 @@ class Commande (BaseModel):
     Attribute
     ---------
     id_commande : int
+                Permet d'identifier une commande
 
     date : str
+            permet de connapitre la date à laquelle la commande a été effectuée
 
     statut_commande : str
+            Permet de connaître le statut de la commande. Les statut d'une commande 
+            dans notre application sont :  enregistrée, en préparation, en livraison, livrée
+    
+    liste_menu : list 
+            Permet de connaître la liste des menus dans une commande
+
+    Méthodes
+    -------
+    prix_total (): 
+        Cette fonction calcul le prix total d'une commande
+            return : float
+
+
+    ajout_menu() :
+        Cette fonction permet d'ajouter un menu dans notre commande
+            return : renvoie rien 
+
     """
     id_commande : int
     date : str
@@ -23,7 +42,9 @@ class Commande (BaseModel):
     liste_quantite : List[int]
         
     def prix_total(self):
-        """Calcul du prix total
+        """
+        Cette méthode sert donner le prix total d'une commande en fonction des prix des articles
+
         """
         prix_total = 0
         for menu,quantite in zip(self.liste_menu, self.liste_quantite):
@@ -32,6 +53,9 @@ class Commande (BaseModel):
         return prix_total
     
     def ajout_menu(self, menu):
+        """
+        Cette méthode permet l'ajout d'un menu dans la commande
+        """
         self.liste_menu.append(menu)
     
     ## Affichage du contenu de la commande
--- api/metier/menu.py
+++ api/metier/menu.py
@@ -2,6 +2,34 @@ from pydantic import BaseModel
 from api.metier.article import Article
 
 class Menu(BaseModel): 
+    """
+    Cette classe nous permet de definir un menu pour notre client
+
+    Attribute
+    --------
+    id_menu : int 
+            Identoifiant du menu
+
+    nom_menu : str
+            Nom du menu
+
+    article1 : article
+             soit le plat ou la boisson ou le dessert
+
+    article2 : article
+              soit le plat ou la boisson ou le dessert
+
+    article 3 : article
+              soit le plat ou la boisson ou le dessert
+
+    Méthodes
+    ------
+    recup_menu()
+        Permet d'affichier un menu
+
+        return : menu
+        Elle retourne un menu
+    """
     id_menu : int 
     nom : str
     prix : int
