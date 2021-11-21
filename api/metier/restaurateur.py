b3a725ba53d0c228894b457e09db607e2496c73b
Nikiema <zoukou1996@gmail.com>
2021-11-21 16:35:39 +0100

    les mises à jours

--- api/metier/restaurateur.py
+++ api/metier/restaurateur.py
@@ -1,6 +1,26 @@
 from pydantic import BaseModel
 
 class Restaurateur(BaseModel):
+    """
+    La classe restaurateur permet de connaître les information sur le restaurateur
+
+    Attributes
+    --------
+    nom : str
+        Le nom de famille du restaurateur
+
+    prenom : str
+        Le prenom du restaurateur
+
+    identifiant : str
+        Identifiant du restaurateur
+
+    mot_de_pass : str
+        Le mot de passe du restaurateur
+
+    id_restaurant : str
+    Identifiant du restaurant
+    """
     nom: str
     prenom: str
     identifiant: str
