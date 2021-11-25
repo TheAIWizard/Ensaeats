25ddd5ce1ab616a4466b669a313f4f8f0366f1df
Nikiema <zoukou1996@gmail.com>
2021-11-25 22:20:50 +0100

    les mises à jours

--- api/service/avis_service.py
+++ api/service/avis_service.py
@@ -5,7 +5,25 @@ from api.metier.avis import Avis
 
 
 class AvisService:
+    """
+    La classe AvisService sert à communiquer avec notre API. Elle nous permettra de gerer les Avis sur l'API
 
+    Attributes
+    ---------
+
+
+    Méthodes
+    --------
+
+    get_avis_by_id_restaurant  ():
+        Cette méthodes permet de  consulter les avis dans la base de données
+        retun : ???
+    
+    ajout_avis () :
+    La fonction ajout_avis permet d'ajouter un avis dans la base deonnées SQL. 
+
+    return : ??
+    """
     @staticmethod
     def get_avis_by_id_restaurant(id_restaurant: str) -> List[Avis]:
         return AvisDao.find_avis_by_id_restaurant(id_restaurant=id_restaurant)
