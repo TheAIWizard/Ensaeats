92fddd0647579b55bbed01f9c2184d06a0fd2764
RANDRIAMANANA Nathan <id1749@clust-n2.domensai.ecole>
2021-11-25 22:41:51 +0100

    authentification view réglé

--- client/view/authentification_view.py
+++ client/view/authentification_view.py
@@ -24,16 +24,18 @@ class AuthentificationView(AbstractView):
             self.identifiant = input("Entrez votre identifiant client:   ")
             self.mot_de_passe = input("Entrez votre mot de passe:   ")
             #sans erreur d'authentification, on passe à la view suivante
-            try:
-                parametres = {"identifiant_client" : self.identifiant , "mot_de_passe_client" : self.mot_de_passe}
-                self.client=requests.get('http://localhost:5000/clients/',params=parametres).json()
-                #self.client=ClientService.authenticate_and_get_client(identifiant=self.identifiant, password=self.mot_de_passe)
-                AbstractView.session.client = self.client
-                AbstractView.session.identifiant = self.identifiant
-                AbstractView.session.mot_de_passe = self.mot_de_passe
-                return WelcomeView()
-            except Exception:
+            parametres = {"mot_de_passe_client" : self.mot_de_passe}
+            self.client=requests.get('http://localhost:8000/clients/{}'.format(self.identifiant),params=parametres,headers = {'accept': 'application/json',}) 
+             #self.client=ClientService.authenticate_and_get_client(identifiant=self.identifiant, password=self.mot_de_passe)
+            AbstractView.session.client = self.client
+            AbstractView.session.identifiant = self.identifiant
+            AbstractView.session.mot_de_passe = self.mot_de_passe
+            #s'il y a une erreur au niveau de la requête, on retourne la page d'authentification
+            if self.client.status_code!=200:
                 return AuthentificationView()
+            #sinon on retient le json du client et on passe à la page d'accueil
+            self.client=self.client.json()
+            return WelcomeView()
         else:
             self.nom= input("Nom: ")
             self.prenom= input("Prénom: ")
@@ -51,7 +53,7 @@ class AuthentificationView(AbstractView):
                 "mot_de_passe": self.mot_de_passe,
                 "telephone": self.telephone}
             try:
-                self.client=requests.post('http://localhost:5000/clients/',json=self.client_json).json()
+                self.client=requests.post('http://localhost:8000/clients/',json=self.client_json).json()
 
                 AbstractView.session.client = self.client
                 AbstractView.session.nom = self.nom
