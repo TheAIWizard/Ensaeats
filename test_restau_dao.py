34f7d20363ee1f681955f93876ff3275d25b50f1
RANDRIAMANANA Nathan <id1749@clust-n2.domensai.ecole>
2021-11-26 23:04:31 +0100

    repo propre officiel readme en cours

--- test_restau_dao.py
+++
@@ -1,7 +0,0 @@
-from brouillon.DAO.restaurant_DAO import RestaurantDao
-from brouillon.metier.restaurant import Restaurant
-
-test_restau = RestaurantDao()
-
-print(test_restau.find_id_restaurant_by_name('Falafel'))
-print(test_restau.find_restaurant_by_id_restau(id_restaurant= ihhIbISGmRBy06tfwQVYow)) 
