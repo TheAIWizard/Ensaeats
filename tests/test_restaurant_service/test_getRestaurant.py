d6c421dc7dd63b0b0fcde315b182cab468ad49b7
RANDRIAMANANA Nathan <id1749@clust-n2.domensai.ecole>
2021-11-26 03:37:41 +0100

    tests unitaires

--- tests/test_restaurant_service/test_getRestaurant.py
+++ tests/test_restaurant_service/test_getRestaurant.py
@@ -1,21 +1,23 @@
 from unittest import TestCase
+import unittest
 
 from api.service.restaurant_service import RestaurantsService
 import requests
 
-class TestgetRestaurants(TestCase):
-    def test_get_restaurants(self):
+class TestgetRestaurant(TestCase):
+    def test_get_restaurant(self):
         # GIVEN
-
+        id_restaurant='LTy9AUgMnLn8YS21KfFZ8g'
         my_key = "jXH_gWewLB5gj0iJ6i55_TspH58WVWWTsKPZLJZej0SpLycR5Y_MWHnBwb5AcPMAUSYW3ud87VnSkxW2JMIb4xiEduf-KS0HpzEyB8wfWSw-q-Ko8u-38WtiPXFyYXYx"
-        url = "https://api.yelp.com/v3/businesses/search"
+        url = "https://api.yelp.com/v3/businesses/{}".format(id_restaurant)
         headers = {"Authorization" : "Bearer "+my_key}
-        term,location,radius='etudiant','Bruz','10000'
 
-        expected_response = requests.get(url, params={"term": term, "location": location, "limit": 50, "sort_by" : 'rating', "categories":'Restaurants',"radius":radius}, 
-                                headers={'Authorization': "bearer "+my_key}).json()
+        expected_response = requests.get(url, params={"id": id_restaurant}, headers={'Authorization': "bearer "+my_key}).json()
             
         # WHEN
-        response = RestaurantsService.getRestaurants(term=term,location=location,radius=radius)
+        response = RestaurantsService.getRestaurant(id=id_restaurant)
         # THEN
-        self.assertEqual(expected_response, damage)
+        self.assertEqual(expected_response, response)
+
+if __name__=='__main__':
+    unittest.main()
