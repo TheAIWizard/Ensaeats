2e61f6a4b8646daf46c7a0c079f4c8a7d1619f40
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2022-06-27 12:43:14 +0000

    flake8 syntax

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import dotenv
 from fastapi import FastAPI
-from api.controller import restaurant_router, article_router, menu_router, avis_router, restaurateur_router
+from api.controller import restaurant_router, article_router, \
+     menu_router, avis_router, restaurateur_router
 from client.controller import client_router
 from api.controller import commande_router
 dotenv.load_dotenv(override=True)
 
 
-
-app = FastAPI(title='EnsaEats',version='2.0',description="un micro-service ayant pour objectif de rapprocher restaurant et restaurateur"
-              ,contact={'name':'Groupe n°x'})
+app = FastAPI(title='EnsaEats', version='2.0', description="un micro-service\
+    /ayant pour objectif de rapprocher\
+    / restaurant et restaurateur", contact={'name': 'Groupe n°x'})
 
 app.include_router(restaurateur_router.router)
 app.include_router(restaurant_router.router)
 app.include_router(article_router.router)
 app.include_router(menu_router.router)
 app.include_router(client_router.router)
-app.include_router(commande_router.router) 
+app.include_router(commande_router.router)
 app.include_router(avis_router.router)
 
 
